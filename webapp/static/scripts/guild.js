b3ea7c242b3b640cffaeccf1c01770df2b680cb3
Daniel Hampl <danhampl31415@gmail.com>
2019-03-24 16:50:51 +0100

    v0.3.0 added more data

--- webapp/static/scripts/guild.js
+++ webapp/static/scripts/guild.js
@@ -32,7 +32,7 @@ function loadData(id) {
                     },
                     {
                         "targets": [2],
-                        // "visible": false,
+                        "visible": false,
                         // "render": function (data, type, row, meta) {
                         //     var ThisDate = data.format('ddd, DD MMM YYYY HH:mm:ss');
                         //     console.log(ThisDate);
@@ -43,7 +43,7 @@ function loadData(id) {
                         },
                     },
                     {
-                        "targets": [3, 4, 5, 6, 7, 8, 9, 10],
+                        "targets": [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
                         "className": "dt-body-right",
                         "type": "num"
                     },
