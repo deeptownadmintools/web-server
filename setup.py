c8058a325611f94f4700ddb76d34360975797076
Daniel Hampl <danhampl31415@gmail.com>
2019-04-25 21:32:07 +0200

    v0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='DTAT_web_server',
-    version='0.3.4',
+    version='0.4.0',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
